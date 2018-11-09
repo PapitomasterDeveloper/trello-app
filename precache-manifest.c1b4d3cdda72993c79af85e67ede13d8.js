5dd1896e9bcc377c12e4ffbae06cf01141a8486f
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2018-11-09 15:21:32 -0600

    Updates

---
+++ precache-manifest.c1b4d3cdda72993c79af85e67ede13d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aebd19bdd8a4e0e932f0",
+    "url": "/trello-app/static/js/runtime~main.aebd19bd.js"
+  },
+  {
+    "revision": "5492b0b45cc7ac5c0e0c",
+    "url": "/trello-app/static/js/main.5492b0b4.chunk.js"
+  },
+  {
+    "revision": "8de97329fc14be2e07e6",
+    "url": "/trello-app/static/js/1.8de97329.chunk.js"
+  },
+  {
+    "revision": "5492b0b45cc7ac5c0e0c",
+    "url": "/trello-app/static/css/main.c5dca6f6.chunk.css"
+  },
+  {
+    "revision": "c163b6c0cb65aa4fd5add415cabeeeaa",
+    "url": "/trello-app/index.html"
+  }
+];
